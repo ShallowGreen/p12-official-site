a77b477271cde70dc5719394c271080da13ae53e
ShallowGreen <183339728@qq.com>
2022-05-19 18:19:10 +0800

    fix: 修复canvas不显示的bug

--- src/pages/infra/components/InfraGL/index.tsx
+++ src/pages/infra/components/InfraGL/index.tsx
@@ -7,44 +7,39 @@ import {
     CSS2DRenderer
 } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
 import styles from './index.less';
-
+import init from './three';
 interface IInfraGLProps {
 }
 
 const InfraGL: React.FunctionComponent<IInfraGLProps> = (props) => {
     const containerRef = useRef<HTMLDivElement>(null);
-    
+
     useLayoutEffect(() => {
-        // init('test-id');
-        const container = document.getElementById('test-id')
-        const body = document.body;
-        console.log(container);
-        
-        if(!container) return
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        // const container = containerRef.current as HTMLDivElement;
+        // const scene = new THREE.Scene();
+		// 	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        container.appendChild( renderer.domElement );
+		// 	const renderer = new THREE.WebGLRenderer();
+		// 	renderer.setSize( window.innerWidth, window.innerHeight );
+		// 	container.appendChild( renderer.domElement );
 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+		// 	const geometry = new THREE.BoxGeometry();
+		// 	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+		// 	const cube = new THREE.Mesh( geometry, material );
+		// 	scene.add( cube );
 
-        camera.position.z = 5;
+		// 	camera.position.z = 5;
 
-        function animate() {
-            requestAnimationFrame( animate );
+		// 	function animate() {
+		// 		requestAnimationFrame( animate );
 
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+		// 		cube.rotation.x += 0.01;
+		// 		cube.rotation.y += 0.01;
 
-            renderer.render( scene, camera );
-        };
+		// 		renderer.render( scene, camera );
+		// 	};
 
-        animate();
+		// 	animate();
     }, [])
 
     // useLayoutEffect(() => {
@@ -56,8 +51,8 @@ const InfraGL: React.FunctionComponent<IInfraGLProps> = (props) => {
     //     let width = window.innerHeight;
     //     let height = window.innerHeight;
     //     let deerR: THREE.Object3D;
-    //     let dirLight: THREE.Light;
-    //     let pointLight: THREE.Light;
+    //     let dirLight: THREE.DirectionalLight;
+    //     let pointLight: THREE.PointLight;
     //     const sceneL = new THREE.Scene()
 
     //     const frustumSize = 45;
@@ -72,6 +67,10 @@ const InfraGL: React.FunctionComponent<IInfraGLProps> = (props) => {
 
     //     let moveY: number = 0, moveX: number = 0;
 
+    //     const axesHelper = new THREE.AxesHelper(1000);
+    //     scene.add(axesHelper);
+    //     // sceneL.add( axesHelper );
+
     //     const lightGroup = new THREE.Group();
     //     const elementGroup = new THREE.Group();
     //     const elementLGroup = new THREE.Group();
@@ -79,34 +78,53 @@ const InfraGL: React.FunctionComponent<IInfraGLProps> = (props) => {
     //     const renderer = new THREE.WebGLRenderer({ // 渲染器
     //         antialias: true, //抗锯齿
     //     })
+    //     container.appendChild(renderer.domElement);
 
-    //     const canvasDom = document.getElementsByTagName('canvas')[2];
+    //     /**
+    //      * 初始化灯光
+    //     */
+    //     const initLight = function () {
+    //         const light = new THREE.AmbientLight(0xffffff, 0.2); // soft white light
+    //         lightGroup.add(light);
+
+    //         // 移动点光源
+    //         pointLight = new THREE.PointLight(0xffffff, 0.8, 100);
+    //         pointLight.position.set(50, 30, 50);
+    //         lightGroup.add(pointLight);
+
+    //         if (dirLight) return
+    //         dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    //         // dirLight.color = new THREE.Color("rgb(255, 255, 255)");
+    //         dirLight.position.set(- 1, 1.75, 1);
+    //         dirLight.position.multiplyScalar(30);
+    //         lightGroup.add(dirLight);
+
+    //         dirLight.castShadow = true;
+
+    //         dirLight.shadow.mapSize.width = 2048;
+    //         dirLight.shadow.mapSize.height = 2048;
+
+    //         const d = 50;
+
+    //         dirLight.shadow.camera.left = - d;
+    //         dirLight.shadow.camera.right = d;
+    //         dirLight.shadow.camera.top = d;
+    //         dirLight.shadow.camera.bottom = - d;
+
+    //         dirLight.shadow.camera.far = 3500;
+    //         dirLight.shadow.bias = - 0.001;
+    //     }
+
+    //     initLight();
+    //     console.log(container.childNodes[0],'container');
+        
     //     const controls = new OrbitControls(
     //         camera,
-    //         renderer.domElement
-    //     );
-    //     controls.enabled = false
-
-
-    //     container.appendChild(renderer.domElement)
-
-    //     scene.background = new THREE.Color(0xcfdedb)
-    //     sceneL.background = new THREE.Color(0xcfdedb)
+    //         container.childNodes[0] as any
+    //       );
 
     //     scene.position.setY(-20)
     //     sceneL.position.setY(-20)
-
-    //     renderer.setPixelRatio(window.devicePixelRatio); //设置渲染的比例
-    //     renderer.setSize(width, window.innerHeight); //设置渲染的尺寸
-    //     renderer.setScissorTest(true);
-
-    //     const labelRenderer = new CSS2DRenderer();
-    //     labelRenderer.setSize(window.innerWidth, window.innerHeight);
-    //     labelRenderer.domElement.style.position = "absolute";
-    //     labelRenderer.domElement.style.top = "0";
-    //     labelRenderer.domElement.style.pointerEvents = "none";
-    //     container.appendChild(labelRenderer.domElement);
-
     //     // 渲染
     //     const render = function () {
     //         const time = Date.now() * 0.0005;
@@ -134,102 +152,25 @@ const InfraGL: React.FunctionComponent<IInfraGLProps> = (props) => {
     //             renderer.setScissor(moveX - w / 2, height - moveY - w / 2, w, w);
     //             renderer.render(sceneL, camera);
 
-    //             labelRenderer.render(sceneL, camera);
+    //             // labelRenderer.render(sceneL, camera);
     //         }
     //     }
-
+    //     renderer.setPixelRatio(window.devicePixelRatio); //设置渲染的比例
+    //     renderer.setSize(width, window.innerHeight); //设置渲染的尺寸
+    //     renderer.setScissorTest(true);
     //     renderer.setAnimationLoop(render);
 
-    //     const animate = () => {
-    //         requestAnimationFrame(animate);
-    //     }
-
-    //     camera.position.set(0, 6, 30);
-
-    //     animate();
-
-    //     const initPlane = function () {
-    //         const geometry = new THREE.PlaneGeometry(300, 150);
-    //         const material = new THREE.MeshLambertMaterial({ color: 0xdcdcdc, side: THREE.DoubleSide });
-    //         const plane = new THREE.Mesh(geometry, material);
-    //         plane.rotateX(Math.PI * 0.5)
-    //         plane.receiveShadow = true
-    //         elementGroup.add(plane)
-
-    //         const material1 = new THREE.MeshBasicMaterial({ color: 0xdcdcdc, side: THREE.DoubleSide });
-    //         const Lplane = new THREE.Mesh(geometry, material1);
-    //         Lplane.rotateX(Math.PI * 0.5)
-
-    //         elementLGroup.add(Lplane)
-    //     }
-    //     initPlane();
-
-    //     loader
-    //         .load(
-    //             'https://medusa-test.oss-cn-hangzhou.aliyuncs.com/TEST/model/deer.obj',
-    //             function (object) {
-    //                 const deerL = object.children[0];
-
-    //                 deerL.material = new THREE.MeshPhongMaterial({
-    //                     color: 0xe9d8c8
-    //                 })
-    //                 deerL.position.setZ(-6)
-    //                 // deerL.position.setY(-2)
-    //                 deerL.scale.set(0.6, 0.6, 0.6)
-    //                 elementGroup.add(deerL)
-    //                 deerR = deerL.clone()
-    //                 deerR.material = new THREE.MeshBasicMaterial({
-    //                     color: 0x000000,   //颜色
-    //                     wireframe: true,  //是否只显示网格
-    //                     transparent: true,
-    //                     opacity: 0.2,
-    //                 });;
-    //                 elementLGroup.add(deerR)
-    //                 elementGroup.traverse(ele => {
-    //                     if (ele.type !== 'Group') {
-    //                         ele.castShadow = true
-    //                         ele.receiveShadow = true
-    //                     }
-    //                 })
-    //             },
-    //             () => { },
-    //             (error) => console.log(error)
-    //         );
-
-    //     // 鼠标移动事件
-    //     const mouseMove = function (event: any) {
-    //         const y = event.pageY
-    //         const x = event.pageX - ((window.innerWidth - width) / 2)
-    //         moveX = x
-    //         moveY = y
-    //     }
+    //     camera.position.set(0, 6, 30)
 
 
-    //     // 屏幕尺寸变化
-    //     const onWindowResize = () => {
-    //         width = window.innerHeight;
-    //         height = window.innerHeight;
-    //         aspect = width / height;
-    //         camera.left = - frustumSize * aspect / 2;
-    //         camera.right = frustumSize * aspect / 2;
-    //         camera.top = frustumSize / 2;
-    //         camera.bottom = - frustumSize / 2;
-    //         camera.updateProjectionMatrix();
-    //         controls.update(); //更新控制器
-    //         renderer.setSize(window.innerHeight, window.innerHeight);
-    //     }
 
-    //     window.addEventListener("mousemove", mouseMove, false);
-    //     window.addEventListener("resize", onWindowResize, false);
 
     //     return () => {
-    //         window.removeEventListener("mousemove", mouseMove)
-    //         window.removeEventListener("resize", onWindowResize)
     //     }
     // }, [])
 
     return <div className={styles.infraGL} ref={containerRef} id="test-id">
-        
+
     </div>;
 };
 
